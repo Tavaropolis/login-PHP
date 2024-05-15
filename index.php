0394f03571639914812f6aa278f9f37a7df0a68d
Tavaropolis <tavares.ag.gabriel@gmail.com>
2024-05-14 21:13:55 -0300

    paginas criadas 1 versão

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Estilização -->
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100..900&display=swap" rel="stylesheet">
+
+    <title>Login</title>
+</head>
+<body>
+    <main>
+        <div class="main-container">
+            <h1>Login</h1>
+            <form action="" method="post">
+                <label for="user">Usuário</label>
+                <input type="text" name="user" id="">
+                <label for="password">Senha</label>
+                <input type="password" name="password" id="">
+                <button type="submit">Enviar</button>
+            </form>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
