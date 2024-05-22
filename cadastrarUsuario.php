47cfc5a9b68f97a48bf61f716040d6a2d2c577c6
Tavaropolis <tavares.ag.gabriel@gmail.com>
2024-05-21 21:06:50 -0300

    style: mudanças 21/05

---
+++ cadastrarUsuario.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Estilização -->
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="cadastrarUsuario.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100..900&display=swap" rel="stylesheet">
+    <title>Cadastrar Usuário</title>
+</head>
+<body>
+    <h1>Cadastrar usuário</h1>
+</body>
+</html>
--- index.php
+++ index.php
@@ -5,26 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Estilização -->
     <link rel="stylesheet" href="reset.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="index.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100..900&display=swap" rel="stylesheet">
 
     <title>Login</title>
 </head>
 <body>
     <main>
+        <h1>Login</h1>
         <div class="main-container">
-            <h1>Login</h1>
             <form action="" method="post">
-                <label for="user">Usuário</label>
-                <input type="text" name="user" id="">
-                <label for="password">Senha</label>
-                <input type="password" name="password" id="">
-                <button type="submit">Enviar</button>
+                <div class="input-container">
+                    <label for="user">Usuário</label>
+                    <input type="text" name="user" id="">
+                    <label for="password">Senha</label>
+                    <input type="password" name="password" id="">
+                    <a href="" id="esqueci-minha-senha">Esqueci minha senha</a>
+                </div>
+                <button type="submit">Entrar</button>
             </form>
+            <div class="new-account-container">
+                <span>Novo no APMS? </span><a href="./cadastrarUsuario.php">Criar usuario</a>
+            </div>
         </div>
     </main>
-    <footer>
-
-    </footer>
 </body>
 </html>
