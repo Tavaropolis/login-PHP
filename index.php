0fa82ff2b5584aa4473a23a48700daf60fdb58cd
Tavaropolis <tavares.ag.gabriel@gmail.com>
2024-05-28 20:42:48 -0300

    fix: Ícone na página

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Estilização -->
+    <link rel="shortcut icon" href="./assets/iconsPlanner.png" type="image/x-icon">
     <link rel="stylesheet" href="reset.css">
     <link rel="stylesheet" href="index.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100..900&display=swap" rel="stylesheet">
